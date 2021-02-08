d180f1bd8783411348983a7aa8fb17dda7c27cec
zarif007 <zarifhuq786@gmail.com>
2021-02-08 19:22:52 +0600

    Added backend.

---
+++ URL/shortner/views.py
@@ -0,0 +1,31 @@
+import uuid
+
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from .models import Url
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def create(request):
+    if request.method == 'POST':
+        link = request.POST['link']
+        prefix = request.POST['prefix']
+
+        uid = prefix
+        if prefix == '':
+            uid = str(uuid.uuid4())[:5]
+        
+        find = Url.objects.filter(uuid=uid).count()
+        if find: 
+            return HttpResponse('invalid prefix')
+        else:
+            new_url = Url(link=link, uuid=uid)
+            new_url.save()
+            return HttpResponse(uid)
+ 
+def go(request, pk):
+    url_details = Url.objects.get(uuid=pk)
+    return redirect(url_details.link)
